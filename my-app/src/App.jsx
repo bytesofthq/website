14bf3b173275a8b16ae835e9aa91847c672bacda
v9mirza <v9mirza@gmail.com>
2026-05-02 11:01:05 +0000

    fix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,9 +3,9 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './pages/About';
-import Services from './Pages/Services';
-import Products from './Pages/Products';
-import ProjectDetails from './Pages/ProjectDetails';
+import Services from './pages/Services';
+import Products from './pages/Products';
+import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
